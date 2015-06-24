22598539be1593664a91eda64c0de66e742592a7
kakayaki <sinoroyal3053@gmail.com>
2015-06-24 15:29:40 +0800

    test1 2015062401  15:28

--- app/src/main/java/roy/idv/helloandroid/MainActivity.java
+++ app/src/main/java/roy/idv/helloandroid/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
+    // test1 2015062401  15:28
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
